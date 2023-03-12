edc4700d1a819daf115d0ff8df2889849539ccec
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2023-03-12 22:57:46 +0530

    Testing diffuser canny2image

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -34,6 +34,8 @@ from ControlNet.annotator.hed import HEDdetector, nms
 from ControlNet.annotator.openpose import OpenposeDetector
 from ControlNet.annotator.uniformer import UniformerDetector
 from ControlNet.annotator.midas import MidasDetector
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
+from diffusers import UniPCMultistepScheduler
 
 VISUAL_CHATGPT_PREFIX = """Visual ChatGPT is designed to be able to assist with a wide range of text and visual related tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. Visual ChatGPT is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
 
@@ -237,9 +239,13 @@ class image2canny:
 class canny2image:
     def __init__(self, device):
         print("Initialize the canny2image model.")
-        model = create_model('ControlNet/models/cldm_v15.yaml', device=device).to(device)
-        model.load_state_dict(load_state_dict('ControlNet/models/control_sd15_canny.pth', location='cpu'))
-        self.model = model.to(device)
+        controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
+        self.pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
+)
+        self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
+        self.pipe.enable_xformers_memory_efficient_attention()
+        self.pipe.enable_model_cpu_offload()
         self.device = device
         self.ddim_sampler = DDIMSampler(self.model)
         self.ddim_steps = 20
@@ -253,34 +259,43 @@ class canny2image:
         self.a_prompt = 'best quality, extremely detailed'
         self.n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality'
 
+
     def inference(self, inputs):
         print("===>Starting canny2image Inference")
         image_path, instruct_text = inputs.split(",")[0], ','.join(inputs.split(',')[1:])
         image = Image.open(image_path)
-        image = np.array(image)
-        image = 255 - image
-        prompt = instruct_text
-        img = resize_image(HWC3(image), self.image_resolution)
-        H, W, C = img.shape
-        control = torch.from_numpy(img.copy()).float().to(device=self.device) / 255.0
-        control = torch.stack([control for _ in range(self.num_samples)], dim=0)
-        control = einops.rearrange(control, 'b h w c -> b c h w').clone()
-        self.seed = random.randint(0, 65535)
-        seed_everything(self.seed)
-        if self.save_memory:
-            self.model.low_vram_shift(is_diffusing=False)
-        cond = {"c_concat": [control], "c_crossattn": [self.model.get_learned_conditioning([prompt + ', ' + self.a_prompt] * self.num_samples)]}
-        un_cond = {"c_concat": None if self.guess_mode else [control], "c_crossattn": [self.model.get_learned_conditioning([self.n_prompt] * self.num_samples)]}
-        shape = (4, H // 8, W // 8)
-        self.model.control_scales = [self.strength * (0.825 ** float(12 - i)) for i in range(13)] if self.guess_mode else ([self.strength] * 13)  # Magic number. IDK why. Perhaps because 0.825**12<0.01 but 0.826**12>0.01
-        samples, intermediates = self.ddim_sampler.sample(self.ddim_steps, self.num_samples, shape, cond, verbose=False, eta=0., unconditional_guidance_scale=self.scale, unconditional_conditioning=un_cond)
-        if self.save_memory:
-            self.model.low_vram_shift(is_diffusing=False)
-        x_samples = self.model.decode_first_stage(samples)
-        x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
+        image = image[:, :, None]
+        image = np.concatenate([image, image, image], axis=2)
+        canny_image = Image.fromarray(image)
+        # image = np.array(image)
+        # image = 255 - image
+        # prompt = instruct_text
+        # img = resize_image(HWC3(image), self.image_resolution)
+        # H, W, C = img.shape
+        # control = torch.from_numpy(img.copy()).float().to(device=self.device) / 255.0
+        # control = torch.stack([control for _ in range(self.num_samples)], dim=0)
+        # control = einops.rearrange(control, 'b h w c -> b c h w').clone()
+        # self.seed = random.randint(0, 65535)
+        # seed_everything(self.seed)
+        # if self.save_memory:
+        #     self.model.low_vram_shift(is_diffusing=False)
+        # cond = {"c_concat": [control], "c_crossattn": [self.model.get_learned_conditioning([prompt + ', ' + self.a_prompt] * self.num_samples)]}
+        # un_cond = {"c_concat": None if self.guess_mode else [control], "c_crossattn": [self.model.get_learned_conditioning([self.n_prompt] * self.num_samples)]}
+        # shape = (4, H // 8, W // 8)
+        # self.model.control_scales = [self.strength * (0.825 ** float(12 - i)) for i in range(13)] if self.guess_mode else ([self.strength] * 13)  # Magic number. IDK why. Perhaps because 0.825**12<0.01 but 0.826**12>0.01
+        # samples, intermediates = self.ddim_sampler.sample(self.ddim_steps, self.num_samples, shape, cond, verbose=False, eta=0., unconditional_guidance_scale=self.scale, unconditional_conditioning=un_cond)
+        # if self.save_memory:
+        #     self.model.low_vram_shift(is_diffusing=False)
+        # x_samples = self.model.decode_first_stage(samples)
+        # x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
+        #generator = torch.manual_seed(0)
+        print(instruct_text)
+        image = self.pipe(
+    instruct_text, num_inference_steps=20, image=canny_image
+).images[0]
         updated_image_path = get_new_image_name(image_path, func_name="canny2image")
-        real_image = Image.fromarray(x_samples[0])  # get default the index0 image
-        real_image.save(updated_image_path)
+        #real_image = Image.fromarray(x_samples[0])  # get default the index0 image
+        image.save(updated_image_path)
         return updated_image_path
 
 class image2line:
